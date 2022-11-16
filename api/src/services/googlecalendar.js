0087aada3c29b73833e2dfc497901283ea628c47
iambillmccann <bill.mccann@gmail.com>
2022-11-16 15:47:25 -0500

    Mostly working code

--- api/src/services/googlecalendar.js
+++ api/src/services/googlecalendar.js
@@ -1,56 +1,58 @@
-import { oauth2Client, google } from 'googleapis'
-
 export const getEvents = async ({ start, end, code }) => {
-  console.log(start)
-  console.log(end)
-  console.log(code)
+  const { google } = require('googleapis')
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.YOUR_CLIENT_ID,
+    process.env.YOUR_CLIENT_SECRET,
+    process.env.YOUR_REDIRECT_URL
+  )
 
   // ToDo implement error handling when no code is passed
-  // let { tokens } = await oauth2Client.getToken(code)
-  // oauth2Client.setCredentials(tokens)
+  let { tokens } = await oauth2Client.getToken(code)
+  console.log({ tokens })
+  oauth2Client.setCredentials(tokens)
 
-  // const calendar = google.calendar({ version: 'v3', oauth2Client })
-  // const res = await calendar.events.list({
-  //   calendarId: 'primary',
-  //   timeMin: start,
-  //   timeMax: end,
-  //   maxResults: 100,
-  //   singleEvents: true,
-  //   orderBy: 'startTime',
-  // })
+  const calendar = google.calendar({ version: 'v3', auth: oauth2Client })
+  const res = await calendar.events.list({
+    calendarId: 'primary',
+    timeMin: start,
+    timeMax: end,
+    maxResults: 100,
+    singleEvents: true,
+    orderBy: 'startTime',
+  })
 
-  // const events = res.data.items
-  // if (!events || events.length === 0) {
-  //   console.log('No upcoming events found.')
-  //   return
-  // }
+  const calendarEvents = res.data.items
+  if (!calendarEvents || calendarEvents.length === 0) {
+    console.log('No upcoming events found.')
+    return
+  }
 
-  // console.log('Upcoming 100 events:')
-  // const cleanedEvents = events.map((event, i) => {
-  //   const start = event.start.dateTime || event.start.date
-  //   const end = event.end.dateTime || event.end.date
-  //   const cleanEvent = {
-  //     summary: event.summary,
-  //     description: event.description,
-  //     start: start,
-  //     end: end,
-  //   }
-  //   return cleanEvent
-  // })
+  console.log('Upcoming 100 events:')
+  const events = calendarEvents.map((item, i) => {
+    const start = item.start.dateTime || item.start.date
+    const end = item.end.dateTime || item.end.date
+    const event = {
+      summary: item.summary,
+      description: item.description,
+      start: start,
+      end: end,
+    }
+    return event
+  })
 
-  const events = [
-    {
-      summary: 'The First Event',
-      description:
-        'This is an example demo of using the Google calendar API in RedwoodJS',
-      start: '2022-11-14T09:00:00Z',
-      end: '2022-11-14T09:30:00Z',
-    },
-    {
-      summary: 'The Second Event',
-      start: '2022-11-14T14:00:00Z',
-      end: '2022-11-14T15:00:00Z',
-    },
-  ]
+  // const events = [
+  //   {
+  //     summary: 'The First Event',
+  //     description:
+  //       'This is an example demo of using the Google calendar API in RedwoodJS',
+  //     start: '2022-11-14T09:00:00Z',
+  //     end: '2022-11-14T09:30:00Z',
+  //   },
+  //   {
+  //     summary: 'The Second Event',
+  //     start: '2022-11-14T14:00:00Z',
+  //     end: '2022-11-14T15:00:00Z',
+  //   },
+  // ]
   return { code, events }
 }
--- web/src/components/AppointmentsCell/AppointmentsCell.js
+++ web/src/components/AppointmentsCell/AppointmentsCell.js
@@ -1,6 +1,7 @@
 export const QUERY = gql`
   query calendar_demo($start: String!, $end: String!, $code: String!) {
     getEvents(start: $start, end: $end, code: $code) {
+      code
       events {
         summary
         description
@@ -19,14 +20,14 @@ export const Failure = ({ error }) => (
   <div style={{ color: 'red' }}>Error: {error?.message}</div>
 )
 
-export const Success = ({ getEvents, start, end, code }) => {
-  alert(start, end)
-  alert(code)
+export const Success = ({ getEvents }) => {
   return (
-    <ul>
-      {getEvents.events.map((item) => {
-        return <li key={item.id}>{JSON.stringify(item)}</li>
-      })}
-    </ul>
+    <>
+      <ul>
+        {getEvents.events.map((item) => {
+          return <li key={item.id}>{JSON.stringify(item)}</li>
+        })}
+      </ul>
+    </>
   )
 }
