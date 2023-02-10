bd3a94f49db33eccab8f9c86508f98518520eef0
iambillmccann <bill.mccann@gmail.com>
2023-02-10 12:47:09 -0500

    Clean up old code

--- api/src/services/googlecalendar.js
+++ api/src/services/googlecalendar.js
@@ -40,19 +40,5 @@ export const getEvents = async ({ start, end, code }) => {
     return event
   })
 
-  // const events = [
-  //   {
-  //     summary: 'The First Event',
-  //     description:
-  //       'This is an example demo of using the Google calendar API in RedwoodJS',
-  //     start: '2022-11-14T09:00:00Z',
-  //     end: '2022-11-14T09:30:00Z',
-  //   },
-  //   {
-  //     summary: 'The Second Event',
-  //     start: '2022-11-14T14:00:00Z',
-  //     end: '2022-11-14T15:00:00Z',
-  //   },
-  // ]
   return { code, events }
 }
